cc25ac19a2f38fddb93b6bdeb8aa433658db2c20
Milan Koirala <koiralamilon67@gmail.com>
2025-06-17 17:15:13 +0545

    Set up app entry point with ThemeProvider and dynamic theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:light_dark_toggler/pages/home_page.dart';
+import 'package:provider/provider.dart';
+import 'theme/theme_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +19,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage());
+      home: const HomePage(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
+    );
   }
 }
